03afa7feaf67874f612312fcd07ad3afb7767b42
Chris Ferdinandi <hello@gomakethings.com>
2013-03-16 19:48:27 -0400

    Added no-self-pings to functions.php

--- functions.php
+++ functions.php
@@ -112,6 +112,26 @@ function stripTrackback($var) {
 
 
 /* ======================================================================
+ * No-Self-Pings.php
+ * A simple function to prevent self-pings.
+ * Script by WPMU - http://wpmu.org/daily-tip-3-ways-to-remove-wordpress-self-pings/
+ * ====================================================================== */
+
+function no_self_ping( &$links ) {
+    $home = get_option( 'home' );
+    foreach ( $links as $l => $link ) {
+        if ( 0 === strpos( $link, $home ) ) {
+            unset($links[$l]);
+        }
+    }
+}
+add_action( 'pre_ping', 'no_self_ping' );
+
+
+
+
+
+/* ======================================================================
  * HTML-Minify.php
  * A simple PHP function to minify the HTML output from your WordPress site.
  * Script by Damn's Virtual Studio.
