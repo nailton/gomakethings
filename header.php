6b58dd3447a51b18fd5e422c0dff647d1435e797
Chris Ferdinandi <hello@gomakethings.com>
2013-03-20 11:23:36 -0400

    Initial Commit

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 	    <meta name="viewport" content="width=device-width, initial-scale=1">
 
 	    <!-- Stylesheet -->
-	    <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/style-min-03182013.css">
+	    <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/style.css">
 
         <!-- HTML5 Shim for IE 6-8 -->
 	    <!--[if lt IE 9]>    
