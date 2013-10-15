3ec2364e00cc15d9a99fd111116d7562e05fdf5e
Chris Ferdinandi <hello@gomakethings.com>
2013-10-14 22:28:20 -0400

    Removed and combined functions

--- functions.php
+++ functions.php
@@ -1,12 +1,10 @@
 <?php
 
-require_once('functions/feature-test.php'); // Run a feature test
 require_once('functions/load-js.php'); // Load external JS files
 require_once('functions/search-form-shortcode.php'); // A WordPress search form shortcode
 require_once('functions/remove-header-junk.php'); // Remove the unneccessary junk WordPress adds to the header
 require_once('functions/search-exclude.php'); // Exclude pages from your search results
 require_once('functions/remove-trackbacks.php'); // Remove trackbacks from comments
 require_once('functions/no-self-pings.php'); // Prevent self-pings
-require_once('functions/html-minify.php'); // Minify HTML output
 
 ?>
--- functions/load-js.php
+++ functions/load-js.php
@@ -6,6 +6,11 @@
  * ====================================================================== */
 
 function load_theme_js() {
+	// Feature Test (in header)
+	wp_register_script('feature-test', get_template_directory_uri() . '/js/feature-test.min.08192013.js', false, null, false);
+	wp_enqueue_script('feature-test');
+
+	// Theme scripts (in footer)
 	wp_register_script('gmt-js', get_template_directory_uri() . '/js/gmt.min.08212013.js', false, null, true);
 	wp_enqueue_script('gmt-js');
 }
