839b28e7b6f7728bf26a51e39b4cd75ab3fa02eb
Chris Ferdinandi <hello@gomakethings.com>
2013-10-16 11:37:52 -0400

    Added check for post type prior to displaying day/comment info

--- search.php
+++ search.php
@@ -13,9 +13,15 @@
 
 			<header>
 				<h1 class="no-space-bottom"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-				<aside>
-					<p class="text-muted text-center"><time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_time('F j, Y') ?></time> &bull; <a class="text-muted" href="<?php comments_link(); ?>"><?php comments_number( 'Respond', '1 Response', '% Responses' ); ?></a><?php edit_post_link('Edit', ' &bull; ', ''); ?></p>
-				</aside>
+				<?php if ( is_single() ) : ?>
+					<aside>
+						<p class="text-muted text-center"><time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_time('F j, Y') ?></time> &bull; <a class="text-muted" href="<?php comments_link(); ?>"><?php comments_number( 'Respond', '1 Response', '% Responses' ); ?></a><?php edit_post_link('Edit', ' &bull; ', ''); ?></p>
+					</aside>
+				<?php else : ?>
+					<aside>
+						<p class="text-muted text-center"><?php edit_post_link('Edit', '', ''); ?></p>
+					</aside>
+				<?php endif; ?>
 			</header>
 
 			<?php the_content('<p>Keep reading...</p>'); ?>
