6efcbd593a1ff06f4fb95b6d2dec84953df6dbe9
Chris Ferdinandi <hello@gomakethings.com>
2013-10-16 12:09:43 -0400

    Removed Google Analytics
    moved to plugin

--- footer.php
+++ footer.php
@@ -10,14 +10,5 @@
 
 		<?php wp_footer(); ?>
 
-
-		<!-- Google Analytics -->
-		<script>
-			var _gaq=[['_setAccount','UA-5423447-9'],['_trackPageview']];
-			(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-			g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
-			s.parentNode.insertBefore(g,s)}(document,'script'));
-		</script>
-
 	</body>
 </html>
