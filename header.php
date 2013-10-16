65cd91e63b41e61f171389e7a8855b4831452a5d
Chris Ferdinandi <hello@gomakethings.com>
2013-10-16 12:59:46 -0400

    Minified stylesheet

--- header.php
+++ header.php
@@ -28,7 +28,7 @@
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
 		<!-- Stylesheet -->
-		<link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/style.css">
+		<link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/style.min.10162013.css">
 
 		<!-- HTML5 Shim for IE 6-8 -->
 		<!--[if lt IE 9]>
