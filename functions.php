9b5f1c7ae1bb706ba9d6774fcc0e6f2357a8303c
Chris Ferdinandi <hello@gomakethings.com>
2014-03-15 22:41:06 -0400

    Converted from spaces to tabs

--- functions.php
+++ functions.php
@@ -76,25 +76,25 @@ add_filter( 'wp_title', 'kraken_pretty_wp_title', 10, 2 );
 
 // If more than one page exists, return TRUE.
 function is_paginated() {
-    global $wp_query;
-    if ( $wp_query->max_num_pages > 1 ) {
-        return true;
-    } else {
-        return false;
-    }
+	global $wp_query;
+	if ( $wp_query->max_num_pages > 1 ) {
+		return true;
+	} else {
+		return false;
+	}
 }
 
 
 
 // If last post in query, return TRUE.
 function is_last_post($wp_query) {
-    $post_current = $wp_query->current_post + 1;
-    $post_count = $wp_query->post_count;
-    if ( $post_current == $post_count ) {
-        return true;
-    } else {
-        return false;
-    }
+	$post_current = $wp_query->current_post + 1;
+	$post_count = $wp_query->post_count;
+	if ( $post_current == $post_count ) {
+		return true;
+	} else {
+		return false;
+	}
 }
 
 ?>
