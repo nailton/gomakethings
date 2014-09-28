bed583156d32d1f8f6e1d1b560a435ad31b32e58
Chris Ferdinandi <hello@gomakethings.com>
2014-09-28 19:08:02 -0400

    Added lazypipe to GulpJS

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 var gulp = require('gulp');
 var plumber = require('gulp-plumber');
 var clean = require('gulp-clean');
+var lazypipe = require('lazypipe');
 var rename = require('gulp-rename');
 var flatten = require('gulp-flatten');
 var tap = require('gulp-tap');
@@ -19,9 +20,8 @@ var package = require('./package.json');
 // Paths to project folders
 var paths = {
 	output : 'dist/',
-	temp: 'src/temp/',
 	scripts : {
-		input : [ 'src/js/' ],
+		input : [ 'src/js/*' ],
 		output : 'dist/js/'
 	},
 	styles : {
@@ -70,36 +70,29 @@ var banner = {
 		' */'
 };
 
-// Concatenate scripts in subfolders
-gulp.task('concatenate', function() {
-	return gulp.src(paths.scripts.input + '*/')
+// Lint and minify scripts
+gulp.task('scripts', ['clean'], function() {
+
+	var jsTasks = lazypipe()
+		.pipe(header, banner.full, { package : package })
+		.pipe(gulp.dest, paths.scripts.output)
+		.pipe(rename, { suffix: '.min' })
+		.pipe(uglify)
+		.pipe(header, banner.min, { package : package })
+		.pipe(gulp.dest, paths.scripts.output);
+
+	return gulp.src(paths.scripts.input)
 		.pipe(plumber())
 		.pipe(flatten())
-
 		.pipe(tap(function (file, t) {
 			if ( file.stat.isDirectory() ) {
 				var name = file.relative + '.js';
 				return gulp.src(file.path + '/*.js')
 					.pipe(concat(name))
-					.pipe(gulp.dest(paths.temp));
+					.pipe(jsTasks());
 			}
-		}));
-});
-
-// Lint and minify scripts
-gulp.task('scripts', ['clean', 'concatenate'], function() {
-	return gulp.src([
-			paths.scripts.input + '*.js',
-			paths.temp + '**/*.js'
-		])
-		.pipe(plumber())
-		.pipe(flatten())
-		.pipe(header(banner.full, { package : package }))
-		.pipe(gulp.dest(paths.scripts.output))
-		.pipe(rename({ suffix: '.min' }))
-		.pipe(uglify())
-		.pipe(header(banner.min, { package : package }))
-		.pipe(gulp.dest(paths.scripts.output));
+		}))
+		.pipe(jsTasks());
 });
 
 // Process, lint, and minify Sass files
@@ -172,9 +165,7 @@ gulp.task('default', [
 	'clean',
 	'static',
 	'theme',
-	'concatenate',
 	'scripts',
 	'styles',
-	'cleanTemp',
 	'test'
 ]);
