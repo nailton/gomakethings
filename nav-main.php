8970d3348ae19c3a982817ae356db245d56a830d
Chris Ferdinandi <hello@gomakethings.com>
2015-04-23 22:57:57 -0400

    v9.4.0 * Removed `::selection` highlighting. * Removed top and side margins from `blockquote` elements. * Updated navigation.

--- nav-main.php
+++ nav-main.php
@@ -19,35 +19,15 @@
 			Menu <span class="nav-toggle-icon">+</span>
 		</a>
 		<div class="nav-menu" id="nav-menu">
-			<!-- <ul class="nav">
-				<li <?php if (is_page('about')) { echo 'class="active"'; }?>>
-					<a href="<?php echo site_url(); ?>/about/"><?php _e( 'About', 'keel' ); ?></a>
-				</li>
-				<li <?php if (is_page('projects') || ( isset( $post ) && $post->post_parent == '4493' ) ) { echo 'class="active"'; }?>>
-					<a href="<?php echo site_url(); ?>/projects/"><?php _e( 'Work', 'keel' ); ?></a>
-				</li>
-				<li <?php if (is_page('code')) { echo 'class="active"'; } ?>>
-					<a href="<?php echo site_url(); ?>/code/"><?php _e( 'Code', 'keel' ); ?></a>
-				</li>
-				<li <?php if (is_page('talks')) { echo 'class="active"'; }?>>
-					<a href="<?php echo site_url(); ?>/talks/"><?php _e( 'Talks', 'keel' ); ?></a>
-				</li>
-				<li <?php if (is_home() || is_single()) { echo 'class="active"'; }?>>
-					<a href="<?php echo site_url(); ?>/blog/"><?php _e( 'Blog', 'keel' ); ?></a>
-				</li>
-			</ul> -->
 			<ul class="nav">
-				<li <?php if (is_page('consulting')) { echo 'class="active"'; }?>>
-					<a href="<?php echo site_url(); ?>/consulting/"><?php _e( 'Consulting', 'keel' ); ?></a>
-				</li>
-				<li <?php if (is_page('training')) { echo 'class="active"'; }?>>
-					<a href="<?php echo site_url(); ?>/training/"><?php _e( 'Training', 'keel' ); ?></a>
+				<li <?php if (is_front_page()) { echo 'class="active"'; }?>>
+					<a href="<?php echo site_url(); ?>/"><?php _e( 'What I Do', 'keel' ); ?></a>
 				</li>
 				<li <?php if (is_page('about')) { echo 'class="active"'; }?>>
 					<a href="<?php echo site_url(); ?>/about/"><?php _e( 'About', 'keel' ); ?></a>
 				</li>
 				<li <?php if (is_page('code')) { echo 'class="active"'; } ?>>
-					<a href="<?php echo site_url(); ?>/code/"><?php _e( 'Code', 'keel' ); ?></a>
+					<a href="<?php echo site_url(); ?>/code/"><?php _e( 'Free Stuff', 'keel' ); ?></a>
 				</li>
 				<li <?php if (is_home() || is_single()) { echo 'class="active"'; }?>>
 					<a href="<?php echo site_url(); ?>/blog/"><?php _e( 'Blog', 'keel' ); ?></a>
